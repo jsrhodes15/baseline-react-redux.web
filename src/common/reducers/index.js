4384b79ee724c14bc0f57f59a47ccdfa70951a4e
johnrhampton <johnrhampton@gmail.com>
2016-05-11 14:52:18 -0400

    navigation action/reducer

--- src/common/reducers/index.js
+++ src/common/reducers/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 import {combineReducers} from 'redux';
+import {navigation_reducer} from './navigation.reducer';
 import {user_reducer} from './user.reducer';
 
 const rootReducer = combineReducers({
+  navigation_reducer,
   user_reducer
 });
 
