1764d3209ec55b8ff143b4924c2f543d6f830933
johnrhampton <johnrhampton@gmail.com>
2016-05-11 09:08:20 -0400

    UserService login

--- src/common/services/UserService.js
+++ src/common/services/UserService.js
@@ -1,16 +1,18 @@
 'use strict';
 
-var environment = require('../constants/environment.js');
+import {ENVIRONMENT} from '../../common/constants/environment';
 import * as ENDPOINTS from '../constants/endpoints';
+import * as HttpService from '../services/HttpService';
 
 export function login(email, password) {
   return new Promise(
     (resolve, reject) => {
-      var url = environment.API_PATH + ENDPOINTS.USER.LOGIN;
+      var url = ENVIRONMENT.API_PATH + ENDPOINTS.USER.LOGIN;
       var payload = {
         email: email,
         password: password
       };
+
       HttpService.post(url, payload)
         .then((response) => {
           return resolve(response);
