242231e15d52a39b17f2caf2e735594f49ca991f
johnrhampton <johnrhampton@gmail.com>
2016-05-26 21:06:05 -0400

    clean up

--- src/common/services/UserService.js
+++ src/common/services/UserService.js
@@ -1,24 +1,24 @@
 'use strict';
 
-import {ENVIRONMENT} from '../../common/constants/environment';
-import * as ENDPOINTS from '../constants/endpoints';
-import * as HttpService from '../services/HttpService';
+import {API_PATH} from '../../common/constants/environment';
+import {USER} from '../constants/endpoints';
+import {post} from '../services/HttpService';
 
 export function login(email, password) {
   return new Promise(
     (resolve, reject) => {
-      var url = ENVIRONMENT.API_PATH + ENDPOINTS.USER.LOGIN;
+      var url = API_PATH + USER.LOGIN;
       var payload = {
         email: email,
         password: password
       };
 
-      HttpService.post(url, payload)
-        .then((response) => {
+      post(url, payload)
+        .then(response => {
           return resolve(response);
         })
-        .catch((err) => {
-          return reject(err);
+        .catch(error => {
+          return reject(error);
         });
-    })
+    });
 }
