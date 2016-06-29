8f647626a2f4c115ba1f4c50c32b10017292a01f
johnrhampton <johnrhampton@gmail.com>
2016-06-28 20:50:10 -0400

    dev API_PATH

--- config/dev.js
+++ config/dev.js
@@ -2,7 +2,7 @@ var pkg = require('../package.json');
 
 module.exports = {
   'ENV_TYPE': 'dev',
-  'API_PATH': '',
+  'API_PATH': 'https://baseline-sails-api.herokuapp.com',
   'VERSION': 'v ' + pkg.version,
   WEBPACK: {
     preLoaders: [],
