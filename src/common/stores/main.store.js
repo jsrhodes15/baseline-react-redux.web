2037c1a8bd87ba63434270ef7d23fdc7d6505e6b
johnrhampton <johnrhampton@gmail.com>
2016-06-28 20:56:23 -0400

    enabling redux dev tools, store clean up

--- src/common/stores/main.store.js
+++ src/common/stores/main.store.js
@@ -1,21 +1,25 @@
 'use strict';
 
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
 
 import {ENVIRONMENT} from '../constants/environment';
 
-export let createStores = function(initialState) {
+export let createStores = (initialState) => {
+  
   const store = createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunkMiddleware, createLogger())
+    compose(
+      applyMiddleware(thunkMiddleware, createLogger()),
+      window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
   );
 
   // only run hot if not local
-  if (module.hot && ENVIRONMENT.ENV_TYPE !== 'local') {
+  if (module.hot && ENVIRONMENT.ENV_TYPE === 'local') {
     module.hot.accept('../reducers', () => {
       const nextRootReducer = require('../reducers').default;
       store.replaceReducer(nextRootReducer);
