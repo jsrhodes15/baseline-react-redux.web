4aed317b04fe895f75ec83e72ab0061758c5ed0e
johnrhampton <johnrhampton@gmail.com>
2016-07-01 09:30:08 -0400

    wiring up storage initializer

--- src/app.js
+++ src/app.js
@@ -1,17 +1,22 @@
 'use strict';
 
-import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 
 import App from './containers/App';
 import * as main_store from './common/stores/main.store';
+import * as storageInitializer from './common/initializers/storage.initializer';
 
 /**
  * create Redux store
  */
 const store = main_store.createStores();
+
+/**
+ * initialize storage - pass appropriate binding context
+ */
+storageInitializer.initStorage(window);
 
 // Vendor JS
 require('../node_modules/material-design-lite/dist/material.min.js');
