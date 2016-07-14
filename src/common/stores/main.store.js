33e9e9efea3d306ba0e0974dc62512c22a129743
johnrhampton <johnrhampton@gmail.com>
2016-07-14 15:29:53 -0400

    making main redux store aware of environment

--- src/common/stores/main.store.js
+++ src/common/stores/main.store.js
@@ -1,45 +1,47 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import {hashHistory} from 'react-router';
 
-/**
- * import root reducer
- */
-import rootReducer from '../reducers';
+import {ENVIRONMENT} from '../constants/environment';
 
 /**
  * third party middleware
  */
 import {routerMiddleware} from 'react-router-redux'
 import thunkMiddleware from 'redux-thunk';
-import createLogger from 'redux-logger';
+
+/**
+ * import root reducer
+ */
+import rootReducer from '../reducers';
 
 /**
  * create and return the main store
  */
 export default (initialState) => {
-
-  // build redux router middleware
+  /**
+   * build redux router middleware
+   */
   const routingMiddleware = routerMiddleware(hashHistory);
-  
-  const store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-      applyMiddleware(thunkMiddleware, createLogger(), routingMiddleware),
-      // redux dev tools extension
-      window.devToolsExtension ? window.devToolsExtension() : f => f
-    )
-  );
 
   /**
-   * only runs locally - enable webpack hot module replacement for reducers
+   * baseline middleware
    */
-  if (module.hot) {
-    module.hot.accept('../reducers', () => {
-      const nextRootReducer = require('../reducers').default;
-      store.replaceReducer(nextRootReducer);
-    });
+  const baseline_middlewares = [thunkMiddleware, routingMiddleware];
+
+  /**
+   * build store for non-production environments
+   */
+  if (ENVIRONMENT.ENV_TYPE !== 'production') {
+    const dev_store = require('./dev.store');
+    return dev_store.default(initialState, rootReducer, baseline_middlewares);
   }
 
-  return store;
+  /**
+   * return production store
+   */
+  return createStore(
+    rootReducer,
+    initialState,
+    compose(applyMiddleware(...baseline_middlewares))
+  );
 };
