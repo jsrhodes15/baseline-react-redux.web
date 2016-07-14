8e08fae4b4753f23acaf1f278d761ce162a701ad
johnrhampton <johnrhampton@gmail.com>
2016-07-14 13:15:05 -0400

    apply routingMiddleware

--- src/common/stores/main.store.js
+++ src/common/stores/main.store.js
@@ -1,17 +1,31 @@
-'use strict';
-
 import {createStore, applyMiddleware, compose} from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import createLogger from 'redux-logger';
+import {hashHistory} from 'react-router';
+
+/**
+ * import root reducer
+ */
 import rootReducer from '../reducers';
 
+/**
+ * third party middleware
+ */
+import {routerMiddleware} from 'react-router-redux'
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
+
+/**
+ * create and return the main store
+ */
 export default (initialState) => {
+
+  // build redux router middleware
+  const routingMiddleware = routerMiddleware(hashHistory);
   
   const store = createStore(
     rootReducer,
     initialState,
     compose(
-      applyMiddleware(thunkMiddleware, createLogger()),
+      applyMiddleware(thunkMiddleware, createLogger(), routingMiddleware),
       // redux dev tools extension
       window.devToolsExtension ? window.devToolsExtension() : f => f
     )
