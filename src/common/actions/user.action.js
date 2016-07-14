cf3e611959738cd6441eac558a7c2c4dd5f0fcd0
johnrhampton <johnrhampton@gmail.com>
2016-07-14 13:08:20 -0400

    validateProfile, saveProfile, moving routing to store

--- src/common/actions/user.action.js
+++ src/common/actions/user.action.js
@@ -1,13 +1,15 @@
-'use strict';
-
+import {push} from 'react-router-redux';
 import * as userService from '../services/user.service';
-import {changeRoute} from './navigation.action';
+import {KEYS} from '../constants/localStorage';
 
 export const LOGIN_REQUEST = 'LOGIN_REQUEST';
 export const LOGIN_COMPLETE = 'LOGIN_COMPLETE';
 export const LOGOUT = 'LOGOUT';
 export const UPDATE_LOGIN_FIELD = 'UPDATE_LOGIN_FIELD';
 
+/**
+ * attempts to log in a user
+ */
 export function login(email, password) {
   return dispatch => {
 
@@ -15,7 +17,9 @@ export function login(email, password) {
 
     return userService.login(email, password)
       .then(profile => {
+        dispatch(saveProfile(profile));
         dispatch(loginComplete({profile: profile}));
+        dispatch(push('/dashboard'));
       })
       .catch(error => {
         dispatch(loginComplete({error: error}));
@@ -23,6 +27,9 @@ export function login(email, password) {
   }
 }
 
+/**
+ * returns the loginComplete dispatch payload
+ */
 export function loginComplete(payload) {
   return {
     type: LOGIN_COMPLETE,
@@ -31,13 +38,28 @@ export function loginComplete(payload) {
   };
 }
 
+/**
+ * logs a user out of our system
+ */
 export function logout() {
   return dispatch => {
     dispatch({type: LOGOUT});
-    dispatch(changeRoute('/'));
   }
 }
 
+/**
+ * save a profile to device storage system
+ */
+export function saveProfile(profile) {
+  return dispatch => {
+    storage.save({key: KEYS.USER_PROFILE, rawData: profile});
+    dispatch({type: KEYS.USER_PROFILE});
+  }
+}
+
+/**
+ * set the value for a login field (email, password, etc.)
+ */
 export function updateLoginField(key, value) {
   return dispatch => {
     let dispatch_payload = {
@@ -47,5 +69,32 @@ export function updateLoginField(key, value) {
     };
 
     dispatch(dispatch_payload);
+  }
+}
+
+/**
+ * gets profile from storage
+ *  connects socket, dispatches success, and navs to dashboard on success
+ */
+export function validateProfile() {
+  return (dispatch, getState) => {
+
+    return storage.load({key: KEYS.USER_PROFILE, autoSync: false})
+      .then(profile => {
+        let default_route = '/dashboard';
+        if (profile && profile.status === 'authenticated') {
+          // get next routing state - default to /dashboard if next route is not available
+          var routing_location = getState().routing.locationBeforeTransitions || {};
+          var next_route = routing_location.state && routing_location.state.nextPathname || default_route;
+
+          dispatch(loginComplete({profile: profile}));
+          dispatch(push(next_route));
+        }
+      })
+      .catch(error => {
+        console.warn(error);
+        // handle missing profile
+        dispatch(saveProfile({}));
+      });
   }
 }
