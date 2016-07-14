8185308647d5ffa4fdffffcd8cf0676685d28204
johnrhampton <johnrhampton@gmail.com>
2016-07-14 12:19:33 -0400

    default export

--- src/common/stores/main.store.js
+++ src/common/stores/main.store.js
@@ -5,7 +5,7 @@ import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
 
-export const createStores = (initialState) => {
+export default (initialState) => {
   
   const store = createStore(
     rootReducer,
