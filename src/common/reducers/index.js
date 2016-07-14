426cf3217f2a2f69a9e61b7a1bec793c51f5f284
johnrhampton <johnrhampton@gmail.com>
2016-07-14 13:12:58 -0400

    adding routing reducer

--- src/common/reducers/index.js
+++ src/common/reducers/index.js
@@ -1,10 +1,13 @@
 'use strict';
 
 import {combineReducers} from 'redux';
+import {routerReducer} from 'react-router-redux';
+
 import {user_reducer} from './user.reducer';
 
 const rootReducer = combineReducers({
-  user_reducer
+  user_reducer,
+  routing: routerReducer
 });
 
 export default rootReducer;
