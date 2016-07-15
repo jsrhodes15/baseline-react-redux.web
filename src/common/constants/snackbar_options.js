47040f716c8ee5ce01f8b3d2aa69d33c3b705a9d
johnrhampton <johnrhampton@gmail.com>
2016-07-15 10:17:15 -0400

    adding snackbar_options const

---
+++ src/common/constants/snackbar_options.js
@@ -0,0 +1,28 @@
+
+export const ERROR = {
+  background: '#F44336',
+  duration: 5000,
+  showActionButton: true,
+  pos: 'bottom-right'
+};
+
+export const INFO = {
+  background: 'rgb(68, 138, 255)',
+  duration: 5000,
+  showActionButton: false,
+  pos: 'bottom-right'
+};
+
+export const SUCCESS = {
+  background: '#4CAF50',
+  duration: 5000,
+  showActionButton: false,
+  pos: 'bottom-center'
+};
+
+export const WARN = {
+  background: '#FF9800',
+  duration: 10000,
+  showActionButton: true,
+  pos: 'bottom-right'
+};
