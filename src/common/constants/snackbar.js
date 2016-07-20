b0e2b740b1826bd4a29cf7bc89b15ae9d99a6fd6
johnrhampton <johnrhampton@gmail.com>
2016-07-20 15:22:20 -0400

    adjusting for actionType

--- src/common/constants/snackbar.js
+++ src/common/constants/snackbar.js
@@ -1,27 +1,25 @@
 export const ERROR = {
   background: '#F44336',
   duration: 5000,
-  showAction: true,
+  actionType: 'CLOSE',
   pos: 'bottom-right'
 };
 
 export const INFO = {
   background: 'rgb(68, 138, 255)',
   duration: 5000,
-  showAction: false,
   pos: 'bottom-right'
 };
 
 export const SUCCESS = {
   background: '#4CAF50',
   duration: 5000,
-  showAction: false,
   pos: 'bottom-center'
 };
 
 export const WARN = {
   background: '#FF9800',
   duration: 10000,
-  showAction: true,
+  actionType: 'CLOSE',
   pos: 'bottom-right'
 };
