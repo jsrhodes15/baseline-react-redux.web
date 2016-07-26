42c2ba43091c526321937ec48ca814ff34f3318f
johnrhampton <johnrhampton@gmail.com>
2016-07-26 12:11:52 -0400

    webpack config updates, moving under config to follow create-react-app pattern

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,99 @@
+/**
+ * webpack concepts are following facebook's create-react-app
+ */
+var path = require('path');
+var autoprefixer = require('autoprefixer');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+var srcPath = path.resolve(__dirname, '../src');
+var nodeModulesPath = path.join(__dirname, '../node_modules');
+
+module.exports = {
+  devtool: 'eval',
+
+  entry: [
+    path.join(srcPath, '/assets/styles/main.scss'),
+    path.join(srcPath, 'app')
+  ],
+
+  output: {
+    filename: 'app.js',
+    path: __dirname + '/build'
+  },
+
+  resolve: {
+    extensions: ['', '.js'],
+  },
+
+  resolveLoader: {
+    root: nodeModulesPath,
+    moduleTemplates: ['*-loader']
+  },
+
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'eslint',
+        include: srcPath,
+      }
+    ],
+
+    loaders: [
+      {
+        test: /\.js$/,
+        include: srcPath,
+        loader: 'babel',
+        query: require('./babel.dev')
+      },
+
+      {
+        test: /\.css$/,
+        loader: 'style!css!postcss'
+      },
+
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
+      },
+
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+
+      {
+        test: /\.(jpg|png|gif|eot|svg|ttf|woff|woff2)$/,
+        loader: 'file',
+      },
+
+      {
+        test: /\.(mp4|webm)$/,
+        loader: 'url?limit=10000'
+      }
+    ]
+  },
+
+  eslint: {
+    configFile: path.join(__dirname, 'eslint.js'),
+    useEslintrc: false
+  },
+
+  postcss: function() {
+    return [autoprefixer];
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './index.html',
+      favicon: './favicon.ico'
+    }),
+
+    new OpenBrowserPlugin({url: 'http://localhost:8080'}),
+
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"development"' })
+  ]
+};
