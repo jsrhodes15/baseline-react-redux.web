a78db444d2ebebb6f07299b574c94f22275bb8ab
johnrhampton <johnrhampton@gmail.com>
2016-07-26 12:37:12 -0400

    removing dev webpack config

--- config/app.dev.js
+++ config/app.dev.js
@@ -3,25 +3,5 @@ var pkg = require('../package.json');
 module.exports = {
   'ENV_TYPE': 'dev',
   'API_PATH': 'http://localhost:1337',
-  'VERSION': 'v ' + pkg.version,
-
-  /**
-   * environment specific webpack config
-   */
-  WEBPACK: {
-    preLoaders: [
-      {
-        loader: 'source-map-loader',
-        test: /\.js$/
-      },
-
-      {
-        test: /\.js?$/,
-        loader: 'eslint',
-        exclude: /node_modules/
-      }
-    ],
-
-    devtool: 'inline-source-map'
-  }
+  'VERSION': 'v ' + pkg.version
 };
