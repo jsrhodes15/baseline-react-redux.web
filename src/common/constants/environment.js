e9e9f2a2149b5101b0ac6935b8b618b984726238
johnrhampton <johnrhampton@gmail.com>
2016-07-26 13:31:10 -0400

    APP_ENV const

---
+++ src/common/constants/environment.js
@@ -0,0 +1,4 @@
+/**
+ * APP_ENV is bundled by webpack - the output of this module will be a config object
+                                                                                                         */
+export const APP_ENV = window.APP_ENV;
