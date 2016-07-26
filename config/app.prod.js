3c7af878755d64e14b09a4a14bfa7c8ff04e511f
johnrhampton <johnrhampton@gmail.com>
2016-07-26 13:30:55 -0400

    removing webpack from app.prod config

--- config/app.prod.js
+++ config/app.prod.js
@@ -3,14 +3,5 @@ var pkg = require('../package.json');
 module.exports = {
   'ENV_TYPE': 'production',
   'API_PATH': 'https://baseline-sails-api.herokuapp.com',
-  'VERSION': 'v ' + pkg.version,
-
-  /**
-   * environment specific webpack config
-   */
-  WEBPACK: {
-    preLoaders: [],
-
-    devtool: 'eval'
-  }
+  'VERSION': 'v ' + pkg.version
 };
